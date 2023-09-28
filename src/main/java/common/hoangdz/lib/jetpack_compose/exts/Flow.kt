81c100991ee28b614cb9e867e0073abdf5caad27
Hoang <hoangdv@wavez.vn>
2023-09-28 18:10:47 +0700

    update

--- src/main/java/common/hoangdz/lib/jetpack_compose/exts/Flow.kt
+++ src/main/java/common/hoangdz/lib/jetpack_compose/exts/Flow.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
 @Composable
@@ -14,3 +15,8 @@ fun <T> StateFlow<T>.collectWhenResume() =
 fun <T> Flow<T>.collectWhenResume(
     initialValue: T,
 ) = collectAsStateWithLifecycle(initialValue, minActiveState = Lifecycle.State.RESUMED)
+
+fun <T> MutableStateFlow<T>.compareAndSet(update: T) {
+    if (value == update) return
+    value = update
+}
