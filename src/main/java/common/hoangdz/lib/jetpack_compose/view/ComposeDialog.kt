09dd12e123ed3540d7737a037e4577a6df363e05
Hoang <hoangdv@wavez.vn>
2024-04-24 17:33:48 +0700

    update score, setting, shape

--- src/main/java/common/hoangdz/lib/jetpack_compose/view/ComposeDialog.kt
+++ src/main/java/common/hoangdz/lib/jetpack_compose/view/ComposeDialog.kt
@@ -1,14 +1,17 @@
 package common.hoangdz.lib.jetpack_compose.view
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.BasicAlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.window.DialogProperties
 import common.hoangdz.lib.jetpack_compose.exts.SafeModifier
 import common.hoangdz.lib.jetpack_compose.exts.collectWhenResume
@@ -26,14 +29,39 @@ fun ComposeDialog(
 ) {
     val isShow by dialogViewModel.dialogState.collectWhenResume()
     if (!isShow) return
-    AlertDialog(modifier = modifier ?: SafeModifier
-        .padding(16.sdp)
-        .background(
-            Color.White, RoundedCornerShape(8)
-        )
-        .padding(8.sdp), properties = properties, onDismissRequest = {
-        if (onDismissRequest.invoke()) dialogViewModel.dismissDialog()
-    }) {
+    BasicAlertDialog(
+        onDismissRequest = {
+            if (onDismissRequest.invoke()) dialogViewModel.dismissDialog()
+        }, modifier = modifier ?: SafeModifier
+            .padding(16.sdp)
+            .background(
+                Color.White, RoundedCornerShape(8)
+            )
+            .padding(8.sdp), properties = properties
+    ) {
         DialogContent()
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ComposeBottomSheetDialog(
+    sheetViewModel: DialogViewModel,
+    modifier: Modifier? = null,
+    onDismissRequest: () -> Boolean = { true },
+    DialogContent: @Composable () -> Unit
+) {
+    val isShow by sheetViewModel.dialogState.collectWhenResume()
+    if (!isShow) return
+    ModalBottomSheet(onDismissRequest = {
+        if (onDismissRequest.invoke()) sheetViewModel.dismissDialog()
+    }, containerColor = Color.Transparent, shape = RectangleShape, dragHandle = {}) {
+        Column(
+            modifier = (modifier ?: SafeModifier.background(
+                Color.White, RoundedCornerShape(topStartPercent = 8, topEndPercent = 8)
+            )).padding(8.sdp)
+        ) {
+            DialogContent()
+        }
     }
 }
