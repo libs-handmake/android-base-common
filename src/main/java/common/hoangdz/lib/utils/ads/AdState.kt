e96db1b87d59322bf6d535f5bd2942f290ee86ef
Hoang <hoangdv@wavez.vn>
2024-06-13 17:39:26 +0700

    update

--- src/main/java/common/hoangdz/lib/utils/ads/AdState.kt
+++ src/main/java/common/hoangdz/lib/utils/ads/AdState.kt
@@ -3,9 +3,6 @@ package common.hoangdz.lib.utils.ads
 object GlobalAdState {
 
     var showingFullScreenADS = false
-        set(value) {
-            field = value
-        }
 
     var isShowInterForNavigationLastTime = false
 
